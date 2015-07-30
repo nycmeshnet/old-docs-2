6b8eb4ab8b7f46176b7c04de3f48c8bbe1f78f12
Brian Hall <bh@usa.net>
2015-07-30 15:04:28 -0400

    Prezi pdf

--- bash/rename_node.sh
+++ bash/rename_node.sh
@@ -3,4 +3,5 @@ nodeid=$(uci get qmp.node.community_node_id)
 uci set qmp.node.community_id="$devname"
 uci set system.@system[0].hostname="$devname-$nodeid"
 echo $devname-$nodeid > /proc/sys/kernel/hostname
+uci commit
 /etc/init.d/bmx6 restart
